82a20a9fd8f70ac4f210832d9fd2d3d6d1e424bf
austinatividad <austin_natividad@dlsu.edu.ph>
2025-07-06 01:56:52 +0800

    feat: add password policy to prevent password from containing username

--- src/Model/Validator.java
+++ src/Model/Validator.java
@@ -5,6 +5,7 @@ public class Validator {
     
     public static String validateRegistration(String username, String password, String confirmPassword, boolean userNameExists) {
         if (userNameExists) return "Username already Exists!";
+        if (password.toLowerCase().contains(username.toLowerCase())) return "Password should not contain your username!";
         if (!password.equals(confirmPassword)) return "Passwords do not match!";
         if (password.length() < 8) return "Password must be at least 8 characters!";
         if (password.length() > 128) return "Password must be less than 128 characters!";
